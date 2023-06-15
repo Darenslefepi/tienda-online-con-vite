31f1b877a710eefec3b2eac5958cbd2bc53143ba
daresnlefepi <felipe6668.afgg@gmail.com>
2023-06-14 19:10:45 -0500

    Borre lo por defecto para dejar limpio el proyecto y comenzar a trabajar en el nuevo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <div className='bg-orange-400'>
+        Hola mundo bebe, no lo puedo creer
+
+      </div>
+  )
+}
+
+export default App
