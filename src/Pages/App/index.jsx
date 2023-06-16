06e1774d93ebe661926e10110999a76daa343fb9
daresnlefepi <felipe6668.afgg@gmail.com>
2023-06-16 18:26:13 -0500

    Enrutamiento con React DOM

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,22 +1,30 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom' /* Esto conecta las paginas, si estoy en home y doy click en MyOrder, me envia ally */
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
 import MyOrders from '../MyOrders'
-import NotFound from '../NotFound'
 import SignIn from '../Signin'
+import NotFound from '../NotFound'
 import './App.css'
 
-function App() {
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element:  <Home /> },
+    { path: '/my-account', element:  <MyAccount /> },
+    { path: '/my-order', element:  <MyOrder /> },
+    { path: '/my-orders', element:  <MyOrders /> },
+    { path: '/sign-in', element:  <SignIn /> },
+    { path: '/*', element:  <NotFound /> },
+  ])
+  return routes
+};
 
+const App = () => {
+  
   return (
-      <div className='bg-orange-400'>
-        <Home />
-        <MyAccount />
-        <MyOrder />
-        <MyOrders />
-        <NotFound />
-        <SignIn />
-      </div>
+    <BrowserRouter>
+      <AppRoutes />{/* aqui estan encapsuladas todas las rutas */}
+    </BrowserRouter>
   )
 }
 
