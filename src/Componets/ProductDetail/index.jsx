9dde3bd9faa46f19c5416fa7676c182566fa6f17
daresnlefepi <felipe6668.afgg@gmail.com>
2023-06-22 16:54:41 -0500

    Agragado de productos al carrito

--- src/Componets/ProductDetail/index.jsx
+++ src/Componets/ProductDetail/index.jsx
@@ -1,15 +1,33 @@
+import { useContext } from 'react'
 import { XMarkIcon } from '@heroicons/react/24/solid'
+import { ShoppingCartContex } from '../../Context'
 import './styles.css'/* como talwin no puede manejar 360px, toco crear el archivo e importarlo */
 
 const ProductDetail = () => {
+    const context = useContext(ShoppingCartContex)
+
     return (
-        <aside className="product-detail flex flex-col fixed bg-white right-0 border border-black rounded-lg">
+        <aside 
+            className={`${context.isproductDetailOpen ? 'flex' : 'hidden'} product-detail flex-col fixed bg-white right-0 border border-black rounded-lg`}>{/* toca asi para poder combinar comandos talwin con logica */}
             <div className='flex justify-between items-center p-6'>
                 <h2 className='font-medium text-xl'>Detail</h2>
                 <button>
-                    <XMarkIcon className='h-6 w-6 text-black'/>
+                    <XMarkIcon 
+                        className='h-6 w-6 text-black'
+                        onClick={() => context.closeProductDetail()}/>
                 </button>
             </div>
+                <figure className='px-6'>
+                    <img 
+                    className='w-full h-full rounded-lg' 
+                    src={context.productToShow.images} 
+                    alt={context.productToShow.title} />
+                </figure>
+                <p className='flex flex-col p-6'>
+                <span className='font-medium text-2xl mb-2'>${context.productToShow.price}</span>
+                <span className='font-medium text-md'>${context.productToShow.title}</span>
+                <span className='font-light text-sm'>${context.productToShow.description}</span>
+                </p>
         </aside>
     )
 }
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,12 +3,31 @@ import { createContext, useState} from "react";
 export const ShoppingCartContex = createContext()
 
 export const ShoppingCartProvider = ({children}) => {
-    const [count, setCount] = useState(0)
+    // Shopping Cart --- Increment Quantity
+    const [count, setCount] = useState(0)/* para llevar la cuenta del carrito */
+    
+    // Product Detail --- Open/Close
+    const [isproductDetailOpen, setIsproductDetailOpen] = useState(false)/* para llevar el registro si esta abierto o cerrado una card */
+    const openProductDetail = () => setIsproductDetailOpen(true)/* para abrir la informacion lateral al presionar en cualquier lado de la card */
+    const closeProductDetail = () => setIsproductDetailOpen(false)/* para cerrar la informacion adicional al presionar x */
+    
+    // Product Detail --- Show Product
+    const [productToShow, setProductToShow] = useState({})
+
+    // Shopping Cart --- Add Products to Cart
+    const [cartProducts, setCartProducts] = useState([])
 
     return (
         <ShoppingCartContex.Provider value={{
             count,
-            setCount
+            setCount,
+            isproductDetailOpen,
+            openProductDetail,
+            closeProductDetail,
+            productToShow,
+            setProductToShow,
+            cartProducts,
+            setCartProducts
         }}>
             {children}
         </ShoppingCartContex.Provider>
